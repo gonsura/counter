dccc5918de7e00fb94d769e25f657560b72fbd4d
gonsura <iden.cioci@gmail.com>
2023-01-25 14:52:50 +0800

    is good?

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-const countEl = document.querySelector(".count-el");
-const btnIncrement = document.querySelector(".increment-btn");
-const save = document.querySelector(".save");
-const saved = document.querySelector(".saved");
-const total = document.querySelector(".total");
+const select = (selected) => document.querySelector(selected);
+const click = (selector, fu) => selector.addEventListener("click", fu);
+
 const saving = [];
 let count = 0;
 
-function increment() {
-  count++;
-  countEl.innerHTML = count;
-}
+const increment = () => (select(".count-el").innerHTML = ++count);
 
-function saveIt() {
+const saveIt = () => {
   if (count == 0) return;
   saving.push(count);
-  console.log(saving);
-  saved.innerHTML = saving.join(" - ");
-  total.innerHTML = saving.reduce((a, b) => a + b);
-  count = 0;
-  countEl.innerHTML = count;
-}
+  select(".saved").innerHTML = saving.join(" - ");
+  select(".total").innerHTML = saving.reduce((a, b) => a + b);
+  select(".count-el").innerHTML = count = 0;
+};
 
-function click(selector, fu) {
-  selector.addEventListener("click", fu);
-}
-
-click(btnIncrement, increment);
-click(save, saveIt);
+click(select(".increment-btn"), increment);
+click(select(".save"), saveIt);
