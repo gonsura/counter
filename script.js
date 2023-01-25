f1b8db3b669737c48526f5317159f2658143e3ef
gonsura <iden.cioci@gmail.com>
2023-01-25 15:06:52 +0800

    add decrement

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const click = (selector, fu) => selector.addEventListener("click", fu);
 const saving = [];
 let count = 0;
 
-const increment = () => (select(".count-el").innerHTML = ++count);
+const increment = () => {
+  select(".count-el").innerHTML = ++count;
+};
+const decrement = () => {
+  if (count != 0) select(".count-el").innerHTML = --count;
+};
 
 const saveIt = () => {
   if (count == 0) return;
@@ -15,4 +20,5 @@ const saveIt = () => {
 };
 
 click(select(".increment-btn"), increment);
+click(select(".decrement-btn"), decrement);
 click(select(".save"), saveIt);
